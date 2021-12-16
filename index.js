7c4166cd50129bc45420afde017b57e80d223e57
canicemichael <canicemichael@gmail.com>
2021-12-16 10:13:49 +0100

    models

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const users = require('./routes/users');
+
 mongoose.connect(process.env.MONGO_URL)
     .then(()=> console.log('DB connection successful'))
     .catch((err) => {
         console.log(err)
     })
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
+app.use('/api/users', users);
 
 app.listen(process.env.PORT, () => {
     console.log('Backend Server started');
